5ab626e3909dc1e20f2b003168024aed893d7478
AllMightyWeak <72877307+AllMightyWeak@users.noreply.github.com>
2025-10-19 19:02:21 +0300

    Add files via upload

---
+++ lock.sol
@@ -0,0 +1,58 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v4.9.3/contracts/token/ERC721/IERC721.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v4.9.3/contracts/token/ERC721/utils/ERC721Holder.sol";
+
+contract SimpleNFTTransfer is ERC721Holder {
+    event TokenLocked(uint tokenId, address sender);
+
+    /**
+     * @notice Transfer any NFT to this contract
+     * @dev User must approve this contract first using ERC721.approve()
+     */
+    function transferNFTToContract(
+        address _nftContract,
+        uint256 _tokenId
+    ) external returns (bool) {
+        IERC721 nft = IERC721(_nftContract);
+        
+        // Check if user owns the NFT
+        require(nft.ownerOf(_tokenId) == msg.sender, "Not NFT owner");
+        
+        // Transfer NFT to this contract
+        nft.safeTransferFrom(msg.sender, address(this), _tokenId);
+        
+        emit TokenLocked(_tokenId, msg.sender);
+        return true;
+    }
+    
+    /**
+     * @notice Transfer NFT from this contract to any address
+     */
+    function transferNFTFromContract(
+        address _nftContract,
+        address _to,
+        uint256 _tokenId
+    ) external returns (bool) {
+        IERC721 nft = IERC721(_nftContract);
+        
+        // Check if contract owns the NFT
+        require(nft.ownerOf(_tokenId) == address(this), "Contract doesn't own this NFT");
+        
+        // Transfer NFT to destination
+        nft.safeTransferFrom(address(this), _to, _tokenId);
+        
+        return true;
+    }
+    
+    /**
+     * @notice Check NFT ownership
+     */
+    function checkNFTOwnership(
+        address _nftContract, 
+        uint256 _tokenId
+    ) external view returns (address) {
+        return IERC721(_nftContract).ownerOf(_tokenId);
+    }
+}
---
+++ nft.sol
@@ -0,0 +1,36 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.4;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+
+contract MyToken is ERC721URIStorage {
+    uint256 public totalMints = 0;
+
+    uint256 public mintPrice = 1 ether;
+    uint256 public maxSupply = 50;
+    uint256 public maxPerWallet = 5;
+    mapping(address => uint256) public walletMints;
+
+    constructor() ERC721("MyToken", "MTK") {}
+
+    function safeMint(address to, string memory tokenURI) internal {
+        uint256 tokenId = totalMints;
+        totalMints++;
+
+        _safeMint(to, tokenId);
+        _setTokenURI(tokenId, tokenURI);
+    }
+
+    function mintToken(uint256 quantity_, string memory tokenURI) public payable {
+        require(quantity_ * mintPrice == msg.value, "wrong amount sent");
+        require(walletMints[msg.sender] + quantity_ <= maxPerWallet, "mints per wallet exceeded");
+
+        walletMints[msg.sender] += quantity_;
+        safeMint(msg.sender, tokenURI);
+    }
+
+    function getMyWalletMints() public view returns (uint256) {
+        return walletMints[msg.sender];
+    }
+}
