56dcfb08ef230627ab4d2179d7e4823854c339ce
AllMightyWeak <72877307+AllMightyWeak@users.noreply.github.com>
2025-10-24 14:37:47 +0300

    Update nft.sol

--- nft.sol
+++ nft.sol
@@ -1,36 +1,25 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.4;
+pragma solidity ^0.8.3;
 
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
-contract MyToken is ERC721URIStorage {
-    uint256 public totalMints = 0;
+contract MyCoolNFT is ERC721URIStorage { 
 
-    uint256 public mintPrice = 1 ether;
-    uint256 public maxSupply = 50;
-    uint256 public maxPerWallet = 5;
-    mapping(address => uint256) public walletMints;
+    using Counters for Counters.Counter; 
+    Counters.Counter private _tokenIds;
 
-    constructor() ERC721("MyToken", "MTK") {}
-
-    function safeMint(address to, string memory tokenURI) internal {
-        uint256 tokenId = totalMints;
-        totalMints++;
-
-        _safeMint(to, tokenId);
-        _setTokenURI(tokenId, tokenURI);
+    constructor() ERC721("My Cool NFT", "MCNFT") {
     }
 
-    function mintToken(uint256 quantity_, string memory tokenURI) public payable {
-        require(quantity_ * mintPrice == msg.value, "wrong amount sent");
-        require(walletMints[msg.sender] + quantity_ <= maxPerWallet, "mints per wallet exceeded");
+    function createToken(string memory tokenURI) public returns (uint) {
+        _tokenIds.increment();
+        uint256 newItemId = _tokenIds.current();
 
-        walletMints[msg.sender] += quantity_;
-        safeMint(msg.sender, tokenURI);
-    }
+        _mint(msg.sender, newItemId);
+        _setTokenURI(newItemId, tokenURI);
 
-    function getMyWalletMints() public view returns (uint256) {
-        return walletMints[msg.sender];
+        return newItemId;
     }
 }
